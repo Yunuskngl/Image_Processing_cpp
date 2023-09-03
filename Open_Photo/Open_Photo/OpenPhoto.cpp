a73f3ad1ec186654fc4790ae62d4fd13225a95d9
Yunus Kangal <117916959+Yunuskngl@users.noreply.github.com>
2023-09-04 02:55:58 +0300

    Add files via upload

---
+++ Open_Photo/Open_Photo/OpenPhoto.cpp
@@ -0,0 +1,23 @@
+﻿#include <opencv2/core/core.hpp>   //temel kütüphane
+#include <opencv2/highgui/highgui.hpp>  //giriş çıkış işlemleri
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+Mat orgImg;
+int main() {
+	orgImg = imread("resim.jpg");
+	namedWindow("Resim Penceresi", WINDOW_AUTOSIZE);
+	if (orgImg.empty()) {
+		cout << "resim yuklenmedi." << endl;
+		return -1;
+	}
+	else {
+		imshow("Resim Penceresi", orgImg);
+		waitKey(0);
+		destroyWindow("Resim Penceresi");
+	}
+	return 0;
+}
